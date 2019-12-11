8fa68c4ed6e9c147c34860bda46974feaa8b72ba
Omega3D <57641901+Omega3D@users.noreply.github.com>
2019-12-11 23:05:53 +0200

    Create labs2.py

---
+++ labs2.py
@@ -0,0 +1,12 @@
+sen = str(input("Write your sentence: "))
+print(len(sen))
+print(sen.count("a"))
+
+
+
+
+sentence = str(input("Write something: "))
+b = set(a.split(" "))
+l = list(b)
+l.sort()
+print(l)
